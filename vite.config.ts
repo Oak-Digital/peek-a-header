00fde9d967d55afbfad204cf7e937b372c563a2a
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-05-11 15:51:44 +0200

    Added demo pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     /* root: 'demo', */
     build: {
         outDir: 'demo-dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                demo: resolve(__dirname, 'demo', 'index.html'),
+                'sticky-extra': resolve(__dirname, 'demo', 'sticky-extra', 'index.html'),
+            }
+        },
     },
 })
